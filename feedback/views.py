b1f81814ee741789db01262df1cb633113976038
Alfatih Ridho NT <52704579+trvlprogrammer@users.noreply.github.com>
2022-10-27 16:11:03 +0700

    pytest and requirements.txt

--- feedback/views.py
+++ feedback/views.py
@@ -7,7 +7,7 @@ from .models import Feature, Feedback, ResUser, Tag
 
 
 def dictfetchall(cursor):
-    "Returns all rows from a cursor as a dict"
+    "function to returns all rows from a cursor as a dict"
     desc = cursor.description
     return [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]
 
--- tests/test_feedback.py
+++ tests/test_feedback.py
@@ -1,5 +1,5 @@
 import pytest
-from feedback.models import Feature, ResUser
+from feedback.models import Feature, Feedback, ResUser, Tag
 
 
 @pytest.mark.django_db
@@ -12,4 +12,23 @@ def test_create_user():
 @pytest.mark.django_db
 def test_create_feature():
     feature = Feature.objects.create(name="new feature")
+    tag1 = Tag.objects.create(name="tag1")
+    tag2 = Tag.objects.create(name="tag2")
+    feature.tag_ids.add(tag1, tag2)
+    assert feature.tag_ids.all()[0].name == "tag1"
     assert feature.name == "new feature"
+
+
+@pytest.mark.django_db
+def test_create_feedback():
+    user = ResUser.objects.create(name="bob", email="bob@mail.com")
+    feature = Feature.objects.create(name="new feature")
+    feature2 = Feature.objects.create(name="new feature2")
+    feature3 = Feature.objects.create(name="new feature3")
+    tag1 = Tag.objects.create(name="tag1")
+    tag2 = Tag.objects.create(name="tag2")
+    feature.tag_ids.add(tag1, tag2)
+    feedback = Feedback.objects.create(feature_id=feature, user_id=user, description="test new feedback")
+    assert feedback.user_id.name == "bob"
+    assert Feature.objects.all().count() == 3
+    assert Feedback.objects.filter(feature_id=feature).count() == 1

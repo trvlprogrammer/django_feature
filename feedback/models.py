a0e6400d8e610a594a51dfd2f80f784638b73670
Alfatih Ridho NT <52704579+trvlprogrammer@users.noreply.github.com>
2022-10-31 12:53:16 +0700

    update contrast font color

--- feedback/models.py
+++ feedback/models.py
@@ -7,9 +7,25 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=50, null=True, blank=True)
+    font_color = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+
+        color_hex = self.color[1:]
+
+        hex_red = int(color_hex[0:2], base=16)
+        hex_green = int(color_hex[2:4], base=16)
+        hex_blue = int(color_hex[4:6], base=16)
+
+        luminance = hex_red * 0.2126 + hex_green * 0.7152 + hex_blue * 0.0722
+        if luminance < 140:
+            self.font_color = "text-white"
+        else:
+            self.font_color = "text-body"
+        super().save(*args, **kwargs)
 
 
 class Feature(models.Model):
